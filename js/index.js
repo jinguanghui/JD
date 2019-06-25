bfe6150f0e59c49929b380b0b239550ae5ddc408
jinguanghui <335208002@qq.com>
2019-06-25 20:39:13 +0800

    ..

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 window.onload=function(){
-//	timeBack();
+	timeBack();
 	searchEffect();
+	
 }
 
 //头部搜索框效果
@@ -10,30 +11,34 @@ function searchEffect(){
 	var bannerHeight = banner.offsetHeight;
 //	console.log(h);
 	window.onscroll=function(){
-		var offsetTop =  document.body.scrollTop;
-		console.log(offsetTop);
-		if(offsetTop<bannerHeight){
-			var opacity= offsetTop/bannerHeight;
+		var offTop = document.body.scrollTop+document.documentElement.scrollTop;
+		console.log(offTop);
+		var opacity=0;
+		if(offTop<bannerHeight){
+		
+			opacity= offTop/bannerHeight;
 			search.style.backgroundColor="rgba(233,35,34,"+opacity+")";
+		}else{
+			
 		}
 	}
 }
 //倒计时
-//function timeBack(){
-//	var spans = document.querySelector(".jd_sk_time").querySelectorAll("span");
-//	var totaltime = 1*60*60;
-//	setInterval(function(){
-//		totaltime--;
-//		var h = Math.floor(totaltime/3600);
-//		var m = Math.floor(totaltime%3600/60);
-//		var s = Math.floor(totaltime%60);
-//		spans[0].innerHTML=Math.floor(h/10);
-//		spans[1].innerHTML=Math.floor(h%10);
-//		
-//		spans[3].innerHTML=Math.floor(m/10);
-//		spans[4].innerHTML=Math.floor(m%10);
-//		
-//		spans[6].innerHTML=Math.floor(s/10);
-//		spans[7].innerHTML=Math.floor(s%10);
-//	},1000);
-//}
+function timeBack(){
+	var spans = document.querySelector(".jd_sk_time").querySelectorAll("span");
+	var totaltime = 1*60*60;
+	setInterval(function(){
+		totaltime--;
+		var h = Math.floor(totaltime/3600);
+		var m = Math.floor(totaltime%3600/60);
+		var s = Math.floor(totaltime%60);
+		spans[0].innerHTML=Math.floor(h/10);
+		spans[1].innerHTML=Math.floor(h%10);
+		
+		spans[3].innerHTML=Math.floor(m/10);
+		spans[4].innerHTML=Math.floor(m%10);
+		
+		spans[6].innerHTML=Math.floor(s/10);
+		spans[7].innerHTML=Math.floor(s%10);
+	},1000);
+}
