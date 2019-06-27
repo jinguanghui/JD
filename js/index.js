e847796f86a490422c343c0ab733e4403758305f
jinguanghui <335208002@qq.com>
2019-06-27 18:12:58 +0800

    提交

--- js/index.js
+++ js/index.js
@@ -10,10 +10,10 @@ function searchEffect(){
 	var banner = document.querySelector(".jd_banner");
 	var bannerHeight = banner.offsetHeight;
 	bannerHeight = bannerHeight/10;
-	console.log(bannerHeight);
+//	console.log(bannerHeight);
 	window.onscroll=function(){
 		var offTop = document.body.scrollTop+document.documentElement.scrollTop;
-		console.log(offTop);
+//		console.log(offTop);
 		var opacity=0;
 		console.log(bannerHeight);
 		if(offTop<(bannerHeight)){
@@ -53,7 +53,7 @@ function bannerEffect(){
 	var first = imageBox.querySelector("li:first-of-type");
 	var last = imageBox.querySelector("li:last-of-type");
 	imageBox.appendChild(first.cloneNode(true));
-	imageBox.insertBefore(first.cloneNode(true),imageBox.firstChild);
+	imageBox.insertBefore(last.cloneNode(true),imageBox.firstChild);
 	
 //	设置样式
 //获取所有的li
@@ -82,7 +82,20 @@ function bannerEffect(){
 		}
 		imageBox.style.left = -index*bannerWidth + "px";
 	}
-	setInterval(function(){
+	
+	 /*实现点标记*/
+   var setIndicator=function(index){
+       var indicators=banner.querySelector("ul:last-of-type").querySelectorAll("li");
+       /*先清除其它li元素的active样式*/
+       for(var i=0;i<indicators.length;i++){
+           indicators[i].classList.remove("active");
+       }
+       /*为当前li元素添加active样式*/
+       indicators[index-1].classList.add("active");
+   }
+   var timerId ;
+	var startTime = function(){
+		timerId = setInterval(function(){
 		 /*5.1 变换索引*/
             index++;
             /*5.2.添加过渡效果*/
@@ -91,7 +104,7 @@ function bannerEffect(){
             imageBox.style.left=(-index*bannerWidth)+"px";
             setTimeout(function(){
             	if(index==count-1){
-                    console.log(index);
+//                  console.log(index);
                     index=1;
                     /*如果一个元素的某个属性之前添加过过渡效果，那么过渡属性会一直存在，如果不想要，则需要清除过渡效果*/
                     /*关闭过渡效果*/
@@ -101,4 +114,74 @@ function bannerEffect(){
                 }
             },500);
 	},2000);
+	}
+	
+	 startTime();
+	
+//	手动轮播
+	var startX,moveX,distenceX;
+	/*标记当前过渡效果是否已经执行完毕*/
+    var isEnd=true;
+	imageBox.addEventListener("touchstart",function(e){
+//		console.log(e);
+		clearInterval(timerId);
+		startX = e.targetTouches[0].clientX;
+		
+	});
+	imageBox.addEventListener("touchmove",function(e){
+		if(isEnd=true){
+			moveX = e.targetTouches[0].clientX;
+			distenceX = moveX - startX;
+			imageBox.style.transition = null;
+			imageBox.style.left = (-index*bannerWidth + distenceX) +"px";
+		}
+		
+	});
+//	松开手指促发
+	imageBox.addEventListener("touchend",function(){
+		/*松开手指，标记当前过渡效果正在执行*/
+        isEnd=false;
+//		判断滑动的距离如果大于100像素实现翻页
+		if(Math.abs(distenceX)>100){
+//			向右滑动索引减一
+			if(distenceX>0){
+				index--;
+			}else{
+				index++;
+			}
+//			翻页,并且实现过度效果
+			imageBox.style.transition="left 0.5s ease-in-out";
+			imageBox.style.left = -index*bannerWidth + "px";
+		}else if(Math.abs(distenceX)>0){//要保证至少有滑动
+//			回弹
+			imageBox.style.transition="left 0.5s ease-in-out";
+			imageBox.style.left = -index*bannerWidth + "px";
+		}
+	});
+	 /*webkitTransitionEnd:可以监听当前元素的过渡效果执行完毕，当一个元素的过渡效果执行完毕的时候，会触发这个事件*/
+	imageBox.addEventListener("webkitTransitionEnd",function(){
+//		console.log("webkitTransitionEnd");
+		 /*如果到了最后一张(count-1)，回到索引1*/
+        /*如果到了第一张(0)，回到索引count-2*/
+		if(index==count-1){
+			index=1;
+			 /*清除过渡*/
+            imageBox.style.transition="none";
+            /*设置偏移*/
+            imageBox.style.left=-index*bannerWidth+"px";
+		} else if(index==0){
+            index=count-2;
+            /*清除过渡*/
+            imageBox.style.transition="none";
+            /*设置偏移*/
+            imageBox.style.left=-index*bannerWidth+"px";
+        }
+		 /*设置标记*/
+        setIndicator(index);
+		setTimeout(function(){
+        	isEnd=true;
+			clearInterval(timerId);
+			startTime();
+		},100);
+	});
 }
